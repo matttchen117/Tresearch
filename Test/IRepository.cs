b1ac56d8e9071b69980e2729d83bfb15212eec57
JesLaz <jelazo@live.com>
2021-12-06 20:53:14 -0800

    Test commit

---
+++ Test/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrialByFire.Tresearch.DAL
+{
+    public interface IRepository<T>
+    {
+        bool Create(T model);
+
+        bool Read();
+
+        bool Update(T model);
+
+        bool Delete(T model);
+
+
+    }
+}
