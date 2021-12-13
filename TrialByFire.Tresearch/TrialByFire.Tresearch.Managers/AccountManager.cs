dff070c2da0e2b9263394add927d9539339b1ecc
JesLaz <jelazo@live.com>
2021-12-12 22:45:50 -0800

    Add AccountManager.cs and AccountService.cs

---
+++ TrialByFire.Tresearch/TrialByFire.Tresearch.Managers/AccountManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TrialByFire.Tresearch.DAO;
+
+namespace TrialByFire.Tresearch.UserManagement
+{
+    public class AccountManager
+    {
+        private MSSQLDAO mssqlDAO;
+        private LogService logService;
+        private Account userAccount;
+
+        public AccountManager(MSSQLDAO mssqlDAO, LogService logService)
+        {
+            this.mssqlDAO = mssqlDAO;
+            this.logService = logService;
+        }
+
+        public bool CreateAccount(string email, string passphrase, string authorizationLevel)
+        {
+        }
+    }
+}
---
+++ TrialByFire.Tresearch/TrialByFire.Tresearch.Services/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialByFire.Tresearch.UserManagement
+{
+    public class AccountService
+    {
+        MSSQLDAO mssqlDAO;
+        LogService logService;
+
+        public AccountService(MSSQLDAO mssqlDAO, LogService logService)
+        {
+            this.mssqlDAO = mssqlDAO;
+            this.logService = logService;
+        }
+
+        public bool CreateAccount(string email, string passphrase, string authorizationLevel)
+        {
+            Account account = new Account(string email, string passphrase, string authorizationLevel);
+            return true;
+        }
+    }
+}
