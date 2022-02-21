132697cade90dad22188945446573ba3109c2282
pammypoor <36018468+pammypoor@users.noreply.github.com>
2022-02-21 00:26:17 -0800

    Removed key

--- CECS 491B/TrialByFire.Tresearch/TrialByFire.Tresearch.Services/MailService.cs
+++ CECS 491B/TrialByFire.Tresearch/TrialByFire.Tresearch.Services/MailService.cs
@@ -7,7 +7,7 @@ namespace TrialByFire.Tresearch.Services
 {
     public class MailService
     {
-        private static string APIKEY = "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";
+        private static string APIKEY = "";
         private static string sender = "no-reply@tresearch.systems";
         private static string senderName = "Tresearch Support";
         private static string confirmationTemplate = "d-debf34255d114608b1af24a054fe1f42";
