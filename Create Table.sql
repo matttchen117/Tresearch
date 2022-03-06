6b0c348ba4558da3ff24b93e3273fa5240c871a1
Drakat7 <55291623+Drakat7@users.noreply.github.com>
2022-03-05 21:20:23 -0800

    Add files via upload

--- Create Table.sql
+++ Create Table.sql
@@ -1,6 +1,4 @@
---CREATE TABLE user_profiles();
-
-CREATE TABLE user_accounts(
+CREATE TABLE useraccounts(
 	username VARCHAR(25),
 	email VARCHAR(40),
 	passphrase VARCHAR(40),
@@ -11,8 +9,8 @@ CREATE TABLE user_accounts(
 	CONSTRAINT user_account_pk PRIMARY KEY(username, authorization_level)
 );
 
-CREATE TABLE otp_claims(
-	username VARCHAR(25) FOREIGN KEY REFERENCES user_accounts(username),
+CREATE TABLE otpclaims(
+	username VARCHAR(25) FOREIGN KEY REFERENCES useraccounts(username),
 	otp VARCHAR(100),
 	time_created TIME,
 	fail_count INT,
@@ -25,64 +23,64 @@ CREATE TABLE nodes(
 	summary VARCHAR(300),
 	mode VARCHAR,
 
-	account_own VARCHAR(25) FOREIGN KEY REFERENCES user_accounts(username)
+	account_own VARCHAR(25) FOREIGN KEY REFERENCES useraccounts(username)
 );
 
 CREATE TABLE tags(
 	tag_name VARCHAR(15) PRIMARY KEY
 );
 
-CREATE TABLE node_tags(
+CREATE TABLE nodetags(
 	node_id BIGINT FOREIGN KEY REFERENCES nodes(node_ID),
 	tag_name VARCHAR(15) FOREIGN KEY REFERENCES tags(tag_name),
 	
 	CONSTRAINT node_tags_pk PRIMARY KEY(node_id, tag_name)
 );
 
-CREATE TABLE user_ratings(
-	username VARCHAR(25) FOREIGN KEY REFERENCES user_accounts(username),
+CREATE TABLE userratings(
+	username VARCHAR(25) FOREIGN KEY REFERENCES useraccounts(username),
 	node_ID BIGINT FOREIGN KEY REFERENCES nodes(node_id),
 	rating INT,
 	
 	CONSTRAINT user_ratings_pk PRIMARY KEY(username, node_id)
 );
 
-CREATE TABLE tree_histories(
+CREATE TABLE treehistories(
 	edit_date DATE,
 	edit_time TIME,
 );
 
-CREATE TABLE views_webpage(
+CREATE TABLE viewswebpage(
 	view_name VARCHAR(25),
 	visits INT,
 	average_duration FLOAT
 );
 
 -- Keep number of registrations on a given date
-CREATE TABLE daily_registrations(
+CREATE TABLE dailyregistrations(
 	registration_date DATE PRIMARY KEY,
 	registration_count INT
 );
 
 -- Keep all of the logins for a given date
-CREATE TABLE daily_login(
+CREATE TABLE dailylogins(
 	login_date DATE PRIMARY KEY,
 	login_count INT
 );
 
-CREATE TABLE top_search(
+CREATE TABLE topsearches(
 	top_search_date DATE PRIMARY KEY,
 	search_string VARCHAR(50),
 	search_count INT
 );
 
-CREATE TABLE nodes_created(
+CREATE TABLE nodescreated(
 	node_creation_date DATE PRIMARY KEY,
 	node_creation_count INT,
 	
 );
 
-CREATE TABLE email_confirmation_links(
+CREATE TABLE emailconfirmationlinks(
 	username VARCHAR(25) PRIMARY KEY,
 	GUID UNIQUEIDENTIFIER,
 	timestamp TIME
