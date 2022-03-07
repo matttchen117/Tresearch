86ae46f48ef6a4ed91cbb104b60516f71592b454
pammypoor <36018468+pammypoor@users.noreply.github.com>
2022-03-06 16:07:23 -0800

    Updated Front end

--- CECS 491B/TrialByFire.Tresearch/client/src/index.js
+++ CECS 491B/TrialByFire.Tresearch/client/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RegistrationForm from './Features/Registration/RegistrationForm.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RegistrationForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
