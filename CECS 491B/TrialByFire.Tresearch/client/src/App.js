c8d9a3def5e8a60c992a06976d727d43e750dc5b
pammypoor <36018468+pammypoor@users.noreply.github.com>
2022-03-20 01:11:56 -0700

    updated client

--- CECS 491B/TrialByFire.Tresearch/client/src/App.js
+++ CECS 491B/TrialByFire.Tresearch/client/src/App.js
@@ -1,14 +1,8 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import RegistrationForm from './Features/Registration/RegistrationForm';
-import ConfirmationSent from './Features/Registration/ConfirmationSent';
-import InactiveLink from './Features/Registration/InactiveLink';
-import LogoutForm from "./Features/Logout/LogoutForm";
-import LoginForm from "./Features/Login/LoginForm";
+
+import Register from "./Features/Registration/Registration";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
@@ -16,11 +10,8 @@ function App() {
       <Router>
         <header></header>
         <Routes>
-          <Route path="/Login/Login" element = {<LoginForm />}/>
-          <Route path="/Logout/Logout" element = {<LogoutForm />}/>
-          <Route path="/Registration/Register" element = {<RegistrationForm />}/>
-          <Route path="/Registration/ConfirmationSent" element = {<ConfirmationSent />}/>
-          <Route path="/Registration/InactiveLink" element = {<InactiveLink />}/>
+        <Route path="/Home" element = {<Home />}/>
+        <Route path="/Registration/Register" element = {<Register />}/>
         </Routes>
       </Router>
     </div>
