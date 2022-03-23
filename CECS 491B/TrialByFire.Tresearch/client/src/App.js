5a1b25db3923f65715ecd064da29dd643d28302d
pammypoor <36018468+pammypoor@users.noreply.github.com>
2022-03-22 21:25:27 -0700

    Updated client side

--- CECS 491B/TrialByFire.Tresearch/client/src/App.js
+++ CECS 491B/TrialByFire.Tresearch/client/src/App.js
@@ -16,15 +16,10 @@ function App() {
       <Router>
         <header></header>
         <Routes>
+          <Route path="/" element = {<Home />}/>
           <Route path="/Login/Login" element = {<LoginForm />}/>
           <Route path="/Login/Authentication" element = {<Authentication />}/>
           <Route path="/Logout/Logout" element = {<LogoutForm />}/>
-          
-          <Route path="/Registration/ConfirmationSent" element = {<ConfirmationSent />}/>
-          <Route path="/Registration/InactiveLink" element = {<InactiveLink />}/>
-          <Route path="/Home" element = {<Home />}/>
-          <Route path="/Registration/Register" element = {<Register />}/>
-          <Route path ="/Tag/Tagger" element = {<Tagger/>} />
           <Route path = "/Recovery/Recover" element = {<Recover/>} />
         </Routes>
       </Router>
--- CECS 491B/TrialByFire.Tresearch/client/src/UI/Navigation/NavigationBar.js
+++ CECS 491B/TrialByFire.Tresearch/client/src/UI/Navigation/NavigationBar.js
@@ -1,26 +1,38 @@
 import React, { useCallback, useState, useEffect } from "react";
+
+
 import logo from './logo.png';
 import Register from "../../Features/Registration/Registration.js";
+import Authentication from "../../Features/Login/Authentication";
+import Login from "../../Features/Login/Login";
 import Popup from "../../UI/Popup/Popup";
 import './NavigationBar.css';
 
 function NavigationBar() {
     const [isSignUpOpen, setIsSignUpOpen] = useState(false);
- 
+    const [isSignInOpen, setIsSignInOpen] = useState(false);
 
-    const NavtoggleSignUp = () => {
+    const NavToggleSignUp = () => {
         setIsSignUpOpen(!isSignUpOpen);
+        if(isSignInOpen){
+            setIsSignInOpen(!isSignInOpen);
+        }
     }
 
-    
+    const NavToggleSignIn = () => {
+        setIsSignInOpen(!isSignInOpen);
+        if(isSignUpOpen){
+            setIsSignUpOpen(!isSignUpOpen);
+        }
+    }
 
     const renderNav = (
         <nav class = "navbar-container">
            <ul className = "nav-links">
-                <li className="logo"><a href="#" ><img src = {logo}/></a></li>
+                <li className="logo"><a href="/" ><img src = {logo}/></a></li>
                 <li className="sec">About</li>
-                <li className="sec"><span onClick={NavtoggleSignUp}>Sign up</span></li>
-                <li className="sec"><span>Sign In</span></li>
+                <li className="sec"><span onClick={NavToggleSignUp}>Sign up</span></li>
+                <li className="sec"><span onClick={NavToggleSignIn}>Sign In</span></li>
             </ul>
         </nav>
     );
@@ -28,7 +40,8 @@ function NavigationBar() {
   return (
     <div className="Home"> 
         {renderNav}
-        {isSignUpOpen && <Popup content = {<Register/> } handleClose = {NavtoggleSignUp}/>}
+        {isSignUpOpen && <Popup content = {<Register/> } handleClose = {NavToggleSignUp}/>}
+        {isSignInOpen && <Popup content = {<Login/> } handleClose = {NavToggleSignIn}/>}
     </div>
   );
 }
