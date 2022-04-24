abb4efe9a1c7b87ddb7f7420f69d5cd64fad5627
Drakat7 <mattchen117@gmail.com>
2022-04-23 23:54:44 -0700

    Search tree navigation works!

--- CECS 491B/TrialByFire.Tresearch/client/src/Features/Search/SearchPage.js
+++ CECS 491B/TrialByFire.Tresearch/client/src/Features/Search/SearchPage.js
@@ -10,89 +10,8 @@ class SearchPage extends React.PureComponent{
 
     this.state = {
       page: new URLSearchParams(window.location.search).get('page'),
-      nodes: [{}]
+      nodes: null
     }
-
-    this.defaultNodes = [{
-      "rootNode": {
-        "children": [
-          {
-            "children": [
-              {
-                "children": [
-                  {
-                    "children": [],
-                    "userHash": "AD89551B3BF5021B53AC0C9878DE96EAB72816241C417DDF2FB421BD78B7B7477372245C5EF36FEEE1A5DB096596D170309A904D9D0FDA6FAD4071148AD67C75",
-                    "nodeID": 339,
-                    "nodeParentID": 5,
-                    "nodeTitle": "Querying",
-                    "summary": "Performing Operations to the Database",
-                    "timeModified": "0001-01-01T00:00:00",
-                    "visibility": true,
-                    "deleted": false,
-                    "exactMatch": false,
-                    "tags": null,
-                    "tagScore": 0,
-                    "ratingScore": 0
-                  }
-                ],
-                "userHash": "AD89551B3BF5021B53AC0C9878DE96EAB72816241C417DDF2FB421BD78B7B7477372245C5EF36FEEE1A5DB096596D170309A904D9D0FDA6FAD4071148AD67C75",
-                "nodeID": 5,
-                "nodeParentID": 3,
-                "nodeTitle": "Intro to SQL",
-                "summary": "Best Language",
-                "timeModified": "0001-01-01T00:00:00",
-                "visibility": true,
-                "deleted": false,
-                "exactMatch": false,
-                "tags": null,
-                "tagScore": 0,
-                "ratingScore": 0
-              }
-            ],
-            "userHash": "AD89551B3BF5021B53AC0C9878DE96EAB72816241C417DDF2FB421BD78B7B7477372245C5EF36FEEE1A5DB096596D170309A904D9D0FDA6FAD4071148AD67C75",
-            "nodeID": 3,
-            "nodeParentID": 2,
-            "nodeTitle": "Intro to HTML/CSS",
-            "summary": "Summary of Hypertext language",
-            "timeModified": "0001-01-01T00:00:00",
-            "visibility": true,
-            "deleted": false,
-            "exactMatch": false,
-            "tags": null,
-            "tagScore": 0,
-            "ratingScore": 0
-          },
-          {
-            "children": [],
-            "userHash": "AD89551B3BF5021B53AC0C9878DE96EAB72816241C417DDF2FB421BD78B7B7477372245C5EF36FEEE1A5DB096596D170309A904D9D0FDA6FAD4071148AD67C75",
-            "nodeID": 4,
-            "nodeParentID": 2,
-            "nodeTitle": "Intro to Javascript",
-            "summary": "Annoying Language",
-            "timeModified": "0001-01-01T00:00:00",
-            "visibility": true,
-            "deleted": false,
-            "exactMatch": false,
-            "tags": null,
-            "tagScore": 0,
-            "ratingScore": 0
-          }
-        ],
-        "userHash": "AD89551B3BF5021B53AC0C9878DE96EAB72816241C417DDF2FB421BD78B7B7477372245C5EF36FEEE1A5DB096596D170309A904D9D0FDA6FAD4071148AD67C75",
-        "nodeID": 2,
-        "nodeParentID": 2,
-        "nodeTitle": "Computer Programming",
-        "summary": "Summary of Computer Programming",
-        "timeModified": "0001-01-01T00:00:00",
-        "visibility": true,
-        "deleted": false,
-        "exactMatch": false,
-        "tags": null,
-        "tagScore": 0,
-        "ratingScore": 0
-      }
-    }]
   }
 
   mapper = (nodeData) => {
@@ -111,32 +30,18 @@ class SearchPage extends React.PureComponent{
     })
   }
 
-  setup = () => {
-    axios.get("https://localhost:7010/TreeManagement/getNodes?owner=" + this.state.page)
-    .then((response => {
-      console.log(response.data)
-      var temp = Object.values(response.data);
-      var temp2 = this.mapper(temp);
-      console.log(temp2[0]);
-      return temp2[0].rootNode;
-      console.log(response.data)
-      this.setState( {nodes: this.mapper(Object.values(response.data))})
-      //console.log(this.state.nodes[0].rootNode)
-      return this.state.nodes[0].rootNode;
-    }))
-    .catch((err => {
-      switch(err.response.status){
-          case 503: {
-                  console.log("Database offline");
-                  //this.setState( {nodes: this.mapper(Object.values(this.defaultNodes))})
-          }
-          break;
-      }
-    }))
-    return this.state.nodes;
+  setup = (n) => {
+    var temp = Object.values(n);
+    var temp2 = this.mapper(temp);
+    return temp2[0];
   }
 
   componentDidMount() {
+    axios.get("https://localhost:7010/TreeManagement/getNodes?owner=" + this.state.page)
+    .then( res => {
+      const nodes = this.setup(res.data);
+      this.setState({nodes});
+    });
   }
   
   render () {
@@ -144,7 +49,9 @@ class SearchPage extends React.PureComponent{
       <div className="Portal-wrapper"> 
         {<NavBar/>}
         <div className = "portal-tree-wrapper">
-          <TreeView nodes = {this.setup()} />
+          {this.state.nodes && (
+            <TreeView nodes = {this.state.nodes} />
+          )}
         </div>
     </div>
     )
